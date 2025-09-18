0692d391d559b1456ffd6314b882ec5a695c8cd5
Eduardo Castilhos <scastilhos.eduardo@gmail.com>
2025-09-18 20:00:17 -0300

    Eduardo: ajuste home

---
+++ js/redirect.js
@@ -0,0 +1,25 @@
+const cardHome1 = document.getElementById('card-home-1');
+const cardHome2 = document.getElementById('card-home-2');
+const cardHome3 = document.getElementById('card-home-3');
+const cardHome4 = document.getElementById('card-home-4');
+const cardHome5 = document.getElementById('card-home-5');
+
+cardHome1.addEventListener('click', () => {
+    window.location.href = 'sobre-ia.html'
+});
+
+cardHome2.addEventListener('click', () => {
+    window.location.href = 'ia-sociedade.html'
+});
+
+cardHome3.addEventListener('click', () => {
+    window.location.href = 'futuro.html'
+});
+
+cardHome4.addEventListener('click', () => {
+    window.location.href = 'conteudos-coticias.html'
+});
+
+cardHome5.addEventListener('click', () => {
+    window.location.href = 'sobre-nos.html'
+});
